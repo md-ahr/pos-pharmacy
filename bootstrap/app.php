7cdbc36542c712953085a89da038015d9954f4a2
Abdul Halim <ahr.web.pro@gmail.com>
2026-07-03 17:40:28 +0600

    feat(deploy): add healthcheck configuration and update nginx settings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'pharmacy.context' => EnsurePharmacyContext::class,
             'privilege' => RequirePrivilege::class,
