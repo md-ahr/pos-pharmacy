b93fdfc3ad91110abf303457bd0ffc8be73ebd5a
Abdul Halim <ahr.web.pro@gmail.com>
2026-07-03 17:18:08 +0600

    feat(deploy): update Docker and supervisord configuration for deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -e
 
-php artisan migrate --force
+export PORT="${PORT:-80}"
+
+envsubst '${PORT}' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
+
+if [ "${SKIP_MIGRATIONS:-}" != "true" ]; then
+    php artisan migrate --force || echo "WARNING: migrations failed, continuing startup"
+fi
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
